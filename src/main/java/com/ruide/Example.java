d28dc555090d10c4ea82e08f7271abf7bd60a930
252350018 <252350018@qq.com>
2020-03-30 11:03:59 +0800

    create angle

--- src/main/java/com/ruide/Example.java
+++ src/main/java/com/ruide/Example.java
@@ -1,9 +1,32 @@
 package com.ruide;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Example {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello");
-        System.out.println("you are my angle");
+    public static void main(String[] args){
+
+        File file = new File("g:/a.txt");
+        if (file.exists()) {
+            System.out.println("this is my exists file");
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            int cout = inputStream.available();
+            System.out.println(cout);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
